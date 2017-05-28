894293140fd7b1814ab534e26475b1a476c662c2
chris sainsbury <chris@sainsbury.im>
2017-05-28 08:58:02 +0100

    .

---
+++ t1_t2_ann.R
@@ -0,0 +1,130 @@
+library(data.table)
+
+returnUnixDateTime<-function(date) {
+  returnVal<-as.numeric(as.POSIXct(date, format="%Y-%m-%d", tz="GMT"))
+  return(returnVal)
+}
+
+findTimeToNearestHbA1c <- function(dmList_LinkId, diagnosisDate_unix) {
+  
+  hb_id_sub <- cleanHbA1cDataDT[LinkId == dmList_LinkId]
+  diffFromDiagnosisDate <- hb_id_sub$dateplustime1 - diagnosisDate_unix
+  
+  findClosest <- sqrt(diffFromDiagnosisDate ^ 2)
+  flagClosest <- ifelse(findClosest == min(findClosest), 1, 0)
+  
+  outputList <- list(findClosest[flagClosest == 1], hb_id_sub$hba1cNumeric[flagClosest == 1])
+  
+  return(outputList)
+  
+}
+
+# set index date
+index <- "2017-01-01"
+paramFromDiagnosisWindowMonths = 6
+paramFromDiagnosisWindowSeconds = paramFromDiagnosisWindowMonths * (60*60*24*(365.25/12))
+
+# diagnosisDataset<-read.csv("../GlCoSy/SDsource/diagnosisDateDeathDate.txt")
+diagnosisDataset<-read.csv("~/R/GlCoSy/SDsource/demogALL.txt", quote = "", 
+                           row.names = NULL, 
+                           stringsAsFactors = FALSE)
+
+diagnosisDatasetDT = data.table(diagnosisDataset)
+
+  # cut down dataset
+  cut_diagDT <- data.table(diagnosisDatasetDT$LinkId, diagnosisDatasetDT$DateOfDiagnosisDiabetes_Date, diagnosisDatasetDT$Ethnicity_Mapped, diagnosisDatasetDT$CurrentGender_Mapped, diagnosisDatasetDT$DiabetesMellitusType_Mapped, diagnosisDatasetDT$BirthDate)
+  colnames(cut_diagDT) <- c("LinkId", "diagnosisDate", "Ethnicity", "Sex", "diabetesType", "DOB")
+  cut_diagDT$LinkId <- as.numeric(cut_diagDT$LinkId)
+  cut_diagDT$LinkId <- as.numeric(cut_diagDT$LinkId)
+  cut_diagDT$DOB_unix <- returnUnixDateTime(cut_diagDT$DOB)
+  
+  # remove those without a diagnosis date
+  cut_diagDT$diagnosisDate_unix <- returnUnixDateTime(cut_diagDT$diagnosisDate)
+  cut_diagDT$diagnosisDate_unix[is.na(cut_diagDT$diagnosisDate_unix)] <- 0
+  cut_diagDT <- cut_diagDT[diagnosisDate_unix > 0]
+  
+  # cut to T1 or T2
+  cut_diagDT <- cut_diagDT[diabetesType == "Type 1 Diabetes Mellitus" | diabetesType == "Type 2 Diabetes Mellitus"]
+  
+  # cut to ensure at least 1y data for each
+  cut_diagDT <- cut_diagDT[diagnosisDate_unix < (returnUnixDateTime(index) - (60*60*24*365.25))]
+
+# generate node and link files
+cleanHbA1cData <- read.csv("~/R/GlCoSy/SD_workingSource/hba1cDTclean.csv", sep=",", header = TRUE, row.names = NULL)
+cleanHbA1cData$timeSeriesDataPoint <- cleanHbA1cData$hba1cNumeric
+  cleanHbA1cDataDT <- data.table(cleanHbA1cData)
+  hb_DT_forMerge <- data.table(cleanHbA1cDataDT$LinkId, cleanHbA1cDataDT$dateplustime1, cleanHbA1cDataDT$hba1cNumeric)
+  colnames(hb_DT_forMerge) <- c("LinkId", "hb_dateplustime1", "hba1cNumeric")
+
+cleanSBPData <- read.csv("~/R/GlCoSy/SD_workingSource/SBPsetDTclean.csv", sep=",", header = TRUE, row.names = NULL)
+cleanSBPData$timeSeriesDataPoint <- cleanSBPData$sbpNumeric
+  cleanSBPDataDT <- data.table(cleanSBPData)
+  sbp_DT_forMerge <- data.table(cleanSBPDataDT$LinkId, cleanSBPDataDT$dateplustime1, cleanSBPDataDT$sbpNumeric)
+  colnames(sbp_DT_forMerge) <- c("LinkId", "sbp_dateplustime1", "sbpNumeric")
+  
+cleanDBPData <- read.csv("~/R/GlCoSy/SD_workingSource/DBPsetDTclean.csv", sep=",", header = TRUE, row.names = NULL)
+cleanDBPData$timeSeriesDataPoint <- cleanDBPData$dbpNumeric
+  cleanDBPDataDT <- data.table(cleanDBPData)
+  dbp_DT_forMerge <- data.table(cleanDBPDataDT$LinkId, cleanDBPDataDT$dateplustime1, cleanDBPDataDT$dbpNumeric)
+  colnames(dbp_DT_forMerge) <- c("LinkId", "dbp_dateplustime1", "dbpNumeric")
+
+cleanBMIData <- read.csv("~/R/GlCoSy/SD_workingSource/BMISetDTclean.csv", sep=",", header = TRUE, row.names = NULL)
+cleanBMIData$timeSeriesDataPoint <- cleanBMIData$bmiNumeric
+  cleanBMIDataDT <- data.table(cleanBMIData)
+  bmi_DT_forMerge <- data.table(cleanBMIDataDT$LinkId, cleanBMIDataDT$dateplustime1, cleanBMIDataDT$bmiNumeric)
+  colnames(bmi_DT_forMerge) <- c("LinkId", "bmi_dateplustime1", "bmiNumeric")
+
+
+# find closest value to diagnosis date for each parameter - sequential merge
+
+# hba1c
+merge_hb <- merge(hb_DT_forMerge, cut_diagDT, by = "LinkId")
+merge_hb[, c("hb_diffFromDiag") := sqrt((hb_dateplustime1 - diagnosisDate_unix) ^ 2) , by=.(LinkId)]
+merge_hb[, c("hb_flagClosest") := ifelse(hb_diffFromDiag == min(hb_diffFromDiag), 1, 0) , by=.(LinkId)]
+
+merge_hb <- merge_hb[(hb_diffFromDiag < paramFromDiagnosisWindowSeconds) & hb_flagClosest == 1]
+
+# SBP
+merge_sbp <- merge(sbp_DT_forMerge, merge_hb, by = "LinkId")
+merge_sbp[, c("sbp_diffFromDiag") := sqrt((sbp_dateplustime1 - diagnosisDate_unix) ^ 2) , by=.(LinkId)]
+merge_sbp[, c("sbp_flagClosest") := ifelse(sbp_diffFromDiag == min(sbp_diffFromDiag), 1, 0) , by=.(LinkId)]
+
+merge_sbp <- merge_sbp[(sbp_diffFromDiag < paramFromDiagnosisWindowSeconds) & sbp_flagClosest == 1]
+
+# # DBP
+# merge_dbp <- merge(dbp_DT_forMerge, merge_sbp, by = "LinkId")
+# merge_dbp[, c("dbp_diffFromDiag") := sqrt((dbp_dateplustime1 - diagnosisDate_unix) ^ 2) , by=.(LinkId)]
+# merge_dbp[, c("dbp_flagClosest") := ifelse(dbp_diffFromDiag == min(dbp_diffFromDiag), 1, 0) , by=.(LinkId)]
+# 
+# merge_dbp <- merge_dbp[(dbp_diffFromDiag < paramFromDiagnosisWindowSeconds) & dbp_flagClosest == 1]
+# merge_dbp <- merge_dbp[sbpNumeric > dbpNumeric]
+
+# BMI
+merge_bmi <- merge(bmi_DT_forMerge, merge_sbp, by = "LinkId")
+merge_bmi[, c("bmi_diffFromDiag") := sqrt((bmi_dateplustime1 - diagnosisDate_unix) ^ 2) , by=.(LinkId)]
+merge_bmi[, c("bmi_flagClosest") := ifelse(bmi_diffFromDiag == min(bmi_diffFromDiag), 1, 0) , by=.(LinkId)]
+
+merge_bmi <- merge_bmi[(bmi_diffFromDiag < paramFromDiagnosisWindowSeconds) & bmi_flagClosest == 1]
+
+# finalset
+diagnostic_test_set <- merge_bmi
+diagnostic_test_set$Sex <- ifelse(diagnostic_test_set$Sex == "Male", 1, 0)
+diagnostic_test_set$diabetesType <- ifelse(diagnostic_test_set$diabetesType == "Type 1 Diabetes Mellitus", 1, 0)
+
+diagnostic_test_set <- diagnostic_test_set[substr(Ethnicity,1,3) != "ECD"]
+diagnostic_test_set$ageAtDiagnosis <- (diagnostic_test_set$diagnosisDate_unix - diagnostic_test_set$DOB_unix) / (60*60*24*365.25)
+
+diagnostic_test_set <- data.table(diagnostic_test_set$ageAtDiagnosis, diagnostic_test_set$Sex, diagnostic_test_set$Ethnicity, diagnostic_test_set$hba1cNumeric, diagnostic_test_set$sbpNumeric, diagnostic_test_set$bmiNumeric, diagnostic_test_set$diabetesType)
+
+colnames(diagnostic_test_set) <- c("age", "sex", "ethnicity", "hba1c", "sbp", "bmi", "diabetesType")
+
+write.table(diagnostic_test_set, file = "./diagSet.csv", sep = ",", row.names = FALSE, col.names = TRUE)
+
+
+
+
+
+
+
+
+cut_diagDT[, c("timeToHbA1c", "HbA1c_value") := findTimeToNearestHbA1c(LinkId, diagnosisDate_unix) , by=.(LinkId)]
